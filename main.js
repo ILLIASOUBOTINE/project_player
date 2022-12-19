aebe0426247cb9ed6e6d2da8ebd79290333661d4
ILLIASOUBOTINE <Semenuk1991311@gmail.com>
2022-12-20 00:19:29 +0100

    thirds commit

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
 const demoPlayer = document.querySelector("audio");
-let src = document.querySelector("source");
 const listAllSongs = document.querySelector("#all_songs");
 const listMySongs = document.querySelector("#my_songs");
 const navTrack = document.querySelector(".nav_track");
-console.dir(listAllSongs);
+const btnAllList = document.querySelector(".btn_all_list");
+const btnMyList = document.querySelector(".btn_my_list");
+const inputAllList = document.querySelector("#btn_all_list");
+const inputMyList = document.querySelector("#btn_my_list");
+const inpVolumeMusique = document.querySelector(".volume_musique");
+
+
+console.dir(inpVolumeMusique);
 console.dir(demoPlayer);
 console.dir(navTrack);
 
-
-// window.onload = function(){
-//     create(arrSongs);
-// } 
-// val.onloadeddata = () => {
-//     console.log(val.duration)
-//   }
-
-// val.loop = true;
-
 let arrUrlSongs = ['5_Nizza-Soldat.mp3','3002-Делить_с_тобой.mp3','basta-mama.mp3'];
 let arrTime = [];
-let arrSongs =[];
+let arrSongs = [];
+let arrMyList = [];
 
 class Song {
-    static numSongActive = 0;
     title;
     duration;
     style;
     lang;
     url;
-    lastNumActive;
+    
     constructor(title, duration,url) {
         this.title = title;
         this.duration = duration;
@@ -36,12 +32,79 @@ class Song {
     }
 }
 
+class MyPlayer {
+    myDemoPlayer;
+    loopSong = false;
+    volume = 0.5;
+    numTrack = 0;
+    currentTime = 0;
+    playList = [];
+    historyList = [];
+
+    constructor(myDemoPlayer, playList) {
+      this.myDemoPlayer = myDemoPlayer;
+      this.playList = playList;
+      this.myDemoPlayer.volume = this.volume;
+    }
+
+    play(){
+        fooPlay(this.myDemoPlayer, this.playList, this.numTrack, this.historyList);
+        this.myDemoPlayer.currentTime = this.currentTime;
+        const setInter1 = setInterval(()=> {
+            if (this.myDemoPlayer.ended && this.playList.length > this.numTrack  ) {
+                this.numTrack++;
+                if(this.playList.length > this.numTrack) {
+                   fooPlay(this.myDemoPlayer, this.playList, this.numTrack, this.historyList); 
+                }else {
+                    clearInterval(setInter1);
+                    this.numTrack = 0;
+                }
+            }    
+        }, 10)
+    }
+
+    stop(){
+        this.myDemoPlayer.pause();
+        this.currentTime = this.myDemoPlayer.currentTime;
+        //  clearInterval(setInter1);
+    }
+
+    reset(){
+        this.currentTime = 0;
+        this.play();
+    }
+
+    loop(){
+        this.loopSong = !this.loopSong;
+        this.myDemoPlayer.loop = this.loopSong;
+    }
+
+    next(){
+        if (this.numTrack < this.playList.length - 1) {
+           ++this.numTrack; 
+        }else { 
+            this.numTrack = 0;
+        }
+        this.reset();
+    }
+
+    prev(){
+        if (this.numTrack > 0) {
+           --this.numTrack; 
+        }else { 
+            this.numTrack = this.playList.length - 1;
+        }
+        this.reset();
+    }
+
+    setVolum(vol){
+        this.volume = vol;
+        this.myDemoPlayer.volume = this.volume;
+    }
 
 
-    
 
-
-
+}
 
 function createSong(arr,arrTime,arrSongs) {
     let i = 0;
@@ -56,19 +119,13 @@ function createSong(arr,arrTime,arrSongs) {
    
 }
 
-
-
 function getDurationSong(arrUrlSongs) {
     let i = 0;
     const setInter = setInterval(()=> {
         let g = document.createElement("audio");
         g.src = `chansons/${arrUrlSongs[i]}`;
         g.onloadeddata = ()=>{
-
-       
         arrTime.push(Math.floor(g.duration));
-           
-            // console.log( Math.round(g.duration));
         };
         i++;
         if (arrUrlSongs.length === i) {
@@ -76,6 +133,28 @@ function getDurationSong(arrUrlSongs) {
         }
     },10);
 }
+
+// async function getDurationSong(arrUrlSongs) {
+   
+//     for(let item of arrUrlSongs) {
+//         let g = document.createElement("audio");
+//         g.src = `chansons/${item}`;
+//         g.onloadeddata = ()=>{
+//         let time = await (Math.floor(g.duration));
+//             arrTime.push(time);
+//         };
+//     }
+// }
+
+// async function getDurSong(item){
+//     let g = document.createElement("audio");
+//     g.src = `chansons/${item}`;
+//     g.onloadeddata = ()=>{
+//       let time = await (Math.floor(g.duration));
+//       arrTime.push(time);
+//     };
+    
+// }
 
 function createItemMusique(parent, title,time ) {
     let div =  document.createElement("div");
@@ -95,10 +174,23 @@ function createItemMusique(parent, title,time ) {
     parent.append(div); 
 }
 
-function fooPlay(demoPlayer,arrSongs,numTrack) {
+function fooPlay(demoPlayer,arrSongs,numTrack,historyList) {
     demoPlayer.src = arrSongs[numTrack].url;
-    // demoPlayer.currentTime = 0;
+    historyList.push(arrSongs[numTrack]);
+    console.log(historyList);
+    document.querySelector(".title_song").textContent = historyList[historyList.length -1].title;
+    
     demoPlayer.play();
+}
+
+function fooConverTime(sec) {
+    let s = sec%60;
+    let m = (sec - s)/60;
+    if(s >= 10){
+        return `${m}:${s}`;
+    }else {
+        return `${m}:0${s}`;
+    }
 }
 
 
@@ -113,6 +205,31 @@ setTimeout(()=>{
        createItemMusique(listAllSongs, item.title, item.duration);
     }
 
+    const player = new MyPlayer(demoPlayer, arrSongs);
+
+    ///////////// trackTime ///////////////
+
+    const trackTime = document.querySelector(".track_time");
+    const trackLine = document.querySelector(".track_line");
+    const trackCircle = document.querySelector(".track_circle");
+    const trackLineRed = document.querySelector(".track_line_red");
+    let coordX = 0;
+    // console.dir(trackLine);
+    setIntervalTrack = setInterval(() => {
+        if (player.myDemoPlayer.currentTime !== 0) {
+            trackTime.textContent = fooConverTime(Math.round(player.myDemoPlayer.duration 
+            - player.myDemoPlayer.currentTime));
+            coordX = trackLine.clientWidth*Math.round(player.myDemoPlayer.currentTime)/Math.round(player.myDemoPlayer.duration);
+            trackCircle.style.left = `${coordX - 25}px`;
+            trackLineRed.style.width = `${coordX - 25}px`;
+        }else {
+            trackTime.textContent = '0:00' ;
+            trackCircle.style.left = `-25px`;
+            trackLineRed.style.width = `0px`;
+        }
+    }, 10)
+
+    ///////////// trackTime ///////////////
     
     listAllSongs.addEventListener("click", (e)=>{
         
@@ -121,6 +238,16 @@ setTimeout(()=>{
             let newItemMusique = elem.parentElement.parentElement.cloneNode(true);
             newItemMusique.lastChild.firstChild.src = "imgs/delete_icon.svg";
             listMySongs.append(newItemMusique);
+            for(let item of arrSongs){
+                if(item.title === newItemMusique.firstChild.textContent){
+                    arrMyList.push(item);
+                    if (arrMyList.length === 1) {
+                        btnMyList.classList.remove("display_hidden");
+                        
+                    }
+                    console.log(arrMyList);
+                } 
+            }
             elem.classList.add("display_hidden");
         }
 
@@ -129,12 +256,14 @@ setTimeout(()=>{
             let elem = e.target;
             for(let item of arrSongs){
                 if(item.title === elem.firstChild.textContent){
+                    document.querySelector(".play").classList.add("display_none");
+                    document.querySelector(".stop").classList.remove("display_none");
+                    player.playList = [item];
+                    player.numTrack = 0;
+                    inputAllList.checked = false;
+                    console.log(player.playList);
                     
-                    item.lastNumActive = ++Song.numSongActive;
-                    console.log(item.lastNumActive);
-                    console.log(Song.numSongActive);
-                    demoPlayer.src = item.url;
-                    demoPlayer.play();
+                    player.reset();
                 } 
              }
         }
@@ -147,49 +276,116 @@ setTimeout(()=>{
             let elem = e.target;
             for(let item of listAllSongs.children){
                if(item.firstChild.textContent === elem.parentElement.previousSibling.textContent){
-                   item.lastChild.firstChild.classList.remove("display_hidden"); 
+                //    console.dir(elem.parentElement.parentElement);
+                    item.lastChild.firstChild.classList.remove("display_hidden"); 
                } 
+               let i = 0;
+               for(let item1 of arrMyList){
+                    if(item1.title === elem.parentElement.previousSibling.textContent){
+                        arrMyList.splice(i,1);
+                        if (arrMyList.length === 0) {
+                            btnMyList.classList.add("display_hidden");
+                        }
+                        // console.log(arrMyList);
+                    } 
+                    i++;
+                }
             }
             elem.parentElement.parentElement.remove();
         }
     });
 
-    let numTrack = 0;
-    let currentTime = 0;
+    
     navTrack.addEventListener("click",(e)=>{
-        // console.log(e.target);
+        // console.dir(e.target.parentElement);
         if (e.target.parentElement.className === "play") {
             document.querySelector(".stop").classList.toggle("display_none");
             e.target.parentElement.classList.toggle("display_none");
-               
-            fooPlay(demoPlayer, arrSongs, numTrack);
-            demoPlayer.currentTime = currentTime;
-            setInter = setInterval(()=> {
-                if (Math.round(demoPlayer.currentTime) >= Math.round(demoPlayer.duration) && arrSongs.length > numTrack ) {
-                    numTrack++;
-                    fooPlay(demoPlayer, arrSongs, numTrack);
-                    
-                    
-                }else if(arrSongs.length === numTrack){
-                    numTrack = 0;
-                    fooPlay(demoPlayer, arrSongs, numTrack);
-                }
-                
-            }, 10)
-           
+            // if(player.playList.length === 0){
+            //     player.playList = [...arrSongs];
+            // }
             
+            console.log(player.playList);
+            player.play() 
         }
 
         if (e.target.parentElement.className === "stop") {
+            // console.dir(e.target.parentElement);
             document.querySelector(".play").classList.toggle("display_none");
             e.target.parentElement.classList.toggle("display_none");
-            demoPlayer.pause();
-            currentTime = demoPlayer.currentTime;
-            clearInterval(setInter);
             
+            player.stop();
         }
 
-    })
+        if (e.target.parentElement.className === "reset") {
+            // console.dir(e.target.parentElement);
+            document.querySelector(".play").classList.add("display_none");
+            document.querySelector(".stop").classList.remove("display_none");
+
+            player.reset();
+        }
+
+        if (e.target.parentElement.classList.contains("loop")) {
+            e.target.parentElement.classList.toggle("active_nav_track");
+           
+            player.loop();
+        }
+
+        if (e.target.parentElement.className === "next") {
+            // console.dir(e.target.parentElement);
+            document.querySelector(".play").classList.add("display_none");
+            document.querySelector(".stop").classList.remove("display_none");
+
+            player.next();
+        }
+
+        if (e.target.parentElement.className === "prev") {
+            // console.dir(e.target.parentElement);
+            document.querySelector(".play").classList.add("display_none");
+            document.querySelector(".stop").classList.remove("display_none");
+
+            player.prev();
+        }
+
+        if (e.target.classList.contains("volume_img")) {
+            // console.dir(e.target.parentElement);
+            document.querySelector(".block_volume").classList.toggle("display_none");
+            e.target.classList.toggle("active_nav_track");
+        }
+
+
+
+    });
+
+    inpVolumeMusique.addEventListener("input", (e)=>{
+        player.setVolum(Number(inpVolumeMusique.value)/100);
+    });
+
+    btnAllList.addEventListener("click", (e)=>{
+        player.playList = arrSongs;
+        player.numTrack = 0;
+        console.log(player.playList);
+                    
+        player.reset();
+    });
+
+    btnMyList.addEventListener("click", (e)=>{
+        if (arrMyList.length > 0) {
+            inputMyList.disabled = false;
+            player.playList = arrMyList;
+            player.numTrack = 0;
+            console.log(player.playList);
+            player.reset();
+        }else {
+            inputMyList.disabled = true;
+            player.playList = arrSongs;
+            inputMyList.checked = false;
+            player.stop();
+            btnMyList.classList.add("display_hidden");
+        }
+    });
+    
+
 }, 1000);
 
 
